bfc678ddf43c70a676d77950bbc3d1d3609ee46b
deerak-isaack <oderaisaack@gmail.com>
2024-07-03 20:06:49 +0300

    removed unnecessary code

--- json_convert.py
+++ json_convert.py
@@ -3,5 +3,6 @@ import pandas as pd
 #Read json data using pandas 
 df = pd.read_json("loan_approval_dataset.json")
 
+
 #Export the dataframe to a csv in the data folder
 df.to_csv("data/loan.csv", index=False)
